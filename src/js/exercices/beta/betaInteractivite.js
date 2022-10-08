1d158326540c2093f77cc41cf7496e04e14a610a
Rémi Angot <remi.angot@gmail.com>
2022-10-08 15:37:11 +0200

    Suppression des listenners utilisés

--- src/js/exercices/beta/betaInteractivite.js
+++ src/js/exercices/beta/betaInteractivite.js
@@ -1,6 +1,6 @@
 import Exercice from '../Exercice.js'
 import { randint, listeQuestionsToContenu } from '../../modules/outils.js'
-import { point, pointAdistance, triangle2points2longueurs } from '../../modules/2d.js'
+import { labelPoint, point, pointAdistance, triangle2points2longueurs } from '../../modules/2d.js'
 import { mathalea2d } from '../../modules/2dGeneralites.js'
 import { context } from '../../modules/context.js'
 export const titre = 'HTMLElement Interactif'
@@ -30,36 +30,39 @@ export default class nomExercice extends Exercice {
       const B = pointAdistance(A, randint(3, 7))
       const triangle = triangle2points2longueurs(A, B, 7, 9)
       const C = triangle.listePoints[2]
+      ;[A.nom, B.nom, C.nom] = 'ABC'.split('')
       if (context.isHtml) {
-        texte = `div : divEx${numeroExercice}Q${i}`
-        texte += `<div id="divEx${numeroExercice}Q${i}"></div>`
+        texte = `<div id="divM2dEx${numeroExercice}Q${i}"></div>` // On place notre div qui n'est qu'un texte tant que l'exercice n'est pas affiché
       } else {
         texte = mathalea2d({ xmin: Math.min(A.x, B.x, C.x), ymin: Math.min(A.y, B.y, C.y), xmax: Math.max(A.x, B.x, C.x), ymax: Math.max(A.y, B.y, C.y) }, triangle)
       }
       texteCorr = ''
 
-      const divM2d = document.createElement('div')
-      divM2d.innerHTML = mathalea2d({ xmin: Math.min(A.x, B.x, C.x), ymin: Math.min(A.y, B.y, C.y), xmax: Math.max(A.x, B.x, C.x), ymax: Math.max(A.y, B.y, C.y) }, triangle)
+      const divM2d = document.createElement('div') // Notre nouveau div de type HTMLElement (et non plus un string)
+      divM2d.innerHTML = mathalea2d({ xmin: Math.min(A.x, B.x, C.x) - 1, ymin: Math.min(A.y, B.y, C.y) - 1, xmax: Math.max(A.x, B.x, C.x) + 1, ymax: Math.max(A.y, B.y, C.y) + 1 }, triangle, labelPoint(A, B, C))
+
+      function insertInDom () {
+        const div = document.querySelector(`#divM2dEx${numeroExercice}Q${i - 1}`) // Quand insertInDom sera exécuté, le i aura été incrémenté d'où ce i - 1
+        if (div) {
+          div.innerHTML = ''
+          div.appendChild(divM2d)
+          const polygons = div.querySelectorAll('polygon')
+          const button = document.createElement('button')
+          button.innerText = 'Polygones en couleur'
+          button.onclick = () => {
+            polygons.forEach(e => { e.setAttribute('stroke', (e.getAttribute('stroke') === 'black') ? 'blue' : 'black') })
+          }
+          div.appendChild(button)
+          button.classList.add('btn', 'ui', 'icon', 'button')
+          document.removeEventListener('exercicesAffiches', insertInDom)
+        }
+      }
+      document.addEventListener('exercicesAffiches', insertInDom)
 
       if (this.questionJamaisPosee(i, B.x, B.y)) {
         this.listeQuestions.push(texte)
         this.listeCorrections.push(texteCorr)
         i++
-        document.addEventListener('exercicesAffiches', () => {
-          const div = document.querySelector(`#divEx${numeroExercice}Q${i - 1}`) // Attention à la synchronisation
-          if (div) {
-            div.innerHTML = ''
-            div.appendChild(divM2d)
-            const button = document.createElement('button')
-            button.innerText = 'Couleur'
-            button.onclick = () => {
-              const polygons = div.querySelectorAll('polygon')
-              polygons.forEach(e => { e.setAttribute('stroke', (e.getAttribute('stroke') === 'black') ? 'blue' : 'black') })
-            }
-            div.appendChild(button)
-            button.classList.add('btn', 'ui', 'icon', 'button')
-          }
-        })
       }
       cpt++
     }
